d3a5bc745bd911f724ebbab2883df54383524273
rqp <noobball111+github@gmail.com>
2026-06-08 09:03:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from Services.Initializer import Init as ServiceLoader
 from Controller.Initializer import Init as ControllerLoader
 
 # The very first time you run this, it creates "Initializer.pyi"
-# Your IDE reads that hidden file and unlocks full autocomplete
+# Your IDE reads that and should enable numtype
 Services = ServiceLoader.load()
 Controller = ControllerLoader.load()
 
+# Originally, it was OrderController.Module if you follow the template, so it could be anything
+# That is why you should assign a ref
 OrderController = Controller.OrderController.OrderController
 
 OrderController.test("abc")
