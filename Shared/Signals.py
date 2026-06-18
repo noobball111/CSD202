71b4b00acb4db8b40d1ece92f5d339b9eff052c9
rqp <noobball111@gmail.com>
2026-06-18 10:41:00 +0700

    Update Signals.py

--- Shared/Signals.py
+++ Shared/Signals.py
@@ -1,13 +1,25 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from Utils.Signal import Signal
 
 @dataclass
+class SearchSignal:
+    BySKU: Signal = Signal.new() 
+    ByUPC: Signal = Signal.new()
+    ByProduct: Signal = Signal.new()
+    ByCategory: Signal = Signal.new()
+    
+
+@dataclass
 class SignalsContainer:
+
+    # Item events
     ItemAdded: Signal = Signal.new()
     ItemRemoving: Signal = Signal.new()
     ItemEdited: Signal = Signal.new()
     OrderProcessed: Signal = Signal.new()
-    
+
+    # Search
+    Search: SearchSignal = field(default_factory=SearchSignal)
 
 # Create shared instance
 Signals = SignalsContainer()
