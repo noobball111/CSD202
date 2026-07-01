4ff138394f85c50a78b9892ddc040c62149947c8
Tracck <85881232+Tracck@users.noreply.github.com>
2026-07-01 09:35:20 +0700

    Revert "push bro quick"
    This reverts commit 55e004eaa623ee442f2c6e1e45f004d3d6b0851b.

--- Classes/Trie.py
+++ Classes/Trie.py
@@ -1,6 +1,5 @@
 from .Stack import Stack
 
-# Each node can have a list of nodes below them instead of just 1
 class Node:
     def __init__(self, val) -> None:
         self.val = val
@@ -12,7 +11,6 @@ class Node:
         self.Words[val] = Node(val)
         return self.Words[val]
 
-# Trie but it's specifically for the autocomplete
 class Trie:
     def __init__(self) -> None:
         self.Head = Node(None)
